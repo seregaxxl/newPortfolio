c1f3f9a7521f5486a75ba03587dc717098fc2875
seregaxxl <seregaxxl@gmail.com>
2023-05-09 21:09:19 -0400

    first screen ready

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { Sidebar } from './Sidebar/Sidebar';
 import { Navibar } from './Navbar/Navbar';
 import { AboutMe } from './Screens/AboutMe/AboutMe';
 
+
 function App() {
   return (
           <>
             <Sidebar></Sidebar>
             <Navibar></Navibar>
             <AboutMe></AboutMe>
+            
           </>
   );
 }
